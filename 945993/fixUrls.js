c888388754b8dc76dc0e6e0d16623710d86c8cf6
AntonDobrev <AntonDobrev@users.noreply.github.com>
2015-07-30 18:04:29 +0300

    add fix for styles requests and for local kendo in the simulator

--- 945993/fixUrls.js
+++ 945993/fixUrls.js
@@ -1,23 +1,52 @@
-if (window.ICE_SimulatorBridge) {
+(function () {
+    if (window.ICE_SimulatorBridge) {
+        var openOriginal = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function () {
+            console.log(arguments[1]);
+            if (arguments[1].indexOf("local://simulator/local://") !== -1) {
+                arguments[1] = arguments[1].replace("local://simulator/local://", "http://");
+            }
 
-	var open = XMLHttpRequest.prototype.open;
-	XMLHttpRequest.prototype.open = function() {
+            openOriginal.apply(this, arguments);
+        };
 
-		if (arguments[1].indexOf("local:") !== -1) {
-			arguments[1] = arguments[1].replace("local://", "http://");
-		}
+        var getProperUrl = function (urlProperty) {
+            if (urlProperty && urlProperty.indexOf("local://") !== -1 && urlProperty.indexOf("local://simulator/") === -1) {
+                urlProperty = urlProperty.replace("local://", "http://");
+            }
+            return urlProperty;
+        };
 
-		open.apply(this, arguments);
-	};
+        ["appendChild", "insertBefore"].forEach(function (methodName) {
+            var originalMethod = Node.prototype[methodName];
+            Node.prototype[methodName] = function () {
+                Array.prototype.forEach.call(arguments, function (arg) {
+                    arg.src = getProperUrl(arg.src);
+                });
 
-	var insertBefore = Node.prototype.insertBefore;
-	Node.prototype.insertBefore = function() {
-		Array.prototype.forEach.call(arguments, function (value) {
-			if(value.src && value.src.indexOf("local://") !== -1 || value.src.indexOf("local://simulator/") !== -1) {
-				value.src = value.src.replace("local://", "http://");
-			}
-		});
+                return originalMethod.apply(this, arguments);
+            };
+        });
 
-		insertBefore.apply(this, arguments);
-	};
-}
+        setInterval(function () {
+            var elements = document.getElementsByClassName("basemapBG");
+            for (var i = 0; i < elements.length; i++) {
+                var el = elements[i];
+                if (el.style && el.style.backgroundImage) {
+                    el.style.backgroundImage = getProperUrl(el.style.backgroundImage);
+                }
+            }
+        }, 2000);
+
+        function getAllElementsWithImageBackground() {
+            var matchingElements = [];
+            var allElements = document.getElementsByTagName('*');
+            for (var i = 0, n = allElements.length; i < n; i++) {
+                if (allElements[i].style && allElements[i].style.backgroundImage) {
+                    matchingElements.push(allElements[i]);
+                }
+            }
+            return matchingElements;
+        }
+    }
+})();
